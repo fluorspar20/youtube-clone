46ee5d6f406df19989833112e27b1eb6c8f9484c
fluorspar20 <amoghdixit104@gmail.com>
2020-06-23 17:02:32 +0530

    layout complete

--- src/App.js
+++ src/App.js
@@ -2,28 +2,66 @@ import React from 'react';
 import './App.css';
 
 import {Grid} from '@material-ui/core'
-import {SearchBar,VideoList, VideoDetail} from './Components'
+import SearchBar from './Components/SearchBar'
+import VideoList from './Components/VideoList'
+import VideoDetail from './Components/VideoDetail'
 
 import youtube from './api/youtube'
 
-function App() {
-  return (
-    <Grid justify="center" container spacing={16}>
-      <Grid item xs={11}>
-        <Grid container spacing={16}>
-          <Grid item xs={12}>
-            { /*searchBar*/ }
-          </Grid>
-          <Grid item xs={8}>
-            { /* video detail*/ }
-          </Grid>
-          <Grid item xs={4}>
-            { /* video suggestions*/ }
+class App extends React.Component{
+
+  state={
+    videos: [],
+    selectedVideo: null
+  }
+
+  componentDidMount(){
+    this.handleSubmit('')
+  }
+
+
+  onVideoSelect = (video) => {
+    this.setState({
+      selectedVideo: video
+    })
+  }
+
+  handleSubmit = async (searchTerm) => {
+    
+    const response = await youtube.get('search', {
+      params: {
+        part: 'snippet',
+        maxResults: 5,
+        key: 'AIzaSyD-AYYRO7bM9eZ1eDaS1OL7KE8K5Aa45Cw',
+        q: searchTerm
+    }
+    })
+
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    })
+  } 
+
+  render(){
+    return (
+      <Grid justify="center" container spacing={10}>
+        <Grid item xs={11}>
+          <Grid container spacing={10}>
+            <Grid item xs={12}>
+              <SearchBar onFormSubmit={this.handleSubmit} />
+            </Grid>
+            <Grid item xs={12} sm={8}>
+              <VideoDetail video={this.state.selectedVideo} />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
-    </Grid>
-  );
+    )
+  }
 }
 
 export default App;
---
+++ src/Components/SearchBar.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import { Paper,TextField} from '@material-ui/core'
+
+class SearchBar extends React.Component{
+
+    state={
+        searchTerm: ''
+    }
+
+    handleChange = (event) => {
+
+        this.setState({
+            searchTerm: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        const {searchTerm} = this.state
+        const {onFormSubmit} = this.props
+
+        onFormSubmit(searchTerm)
+        event.preventDefault()
+    }
+
+    render(){
+        return(
+            <Paper elevation={6} style={{padding: '25px'}}>
+                <img src="/assets/yt-logo.jpg" style={{marginRight: 'auto', marginLeft: 'auto'}} height="70" alt="logo" />
+                <form onSubmit={this.handleSubmit}>
+                    <TextField fullWidth
+                        variant="outlined"
+                        className="search" 
+                        label="Search..."  
+                        onChange= {this.handleChange}
+                    />
+                </form>
+            </Paper>
+        )
+    }
+}
+
+export default SearchBar
